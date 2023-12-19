9a488e0026a5d610899db0028a539a937612776e
TasyaNurulLaily <tasyalaily8@gmail.com>
2023-12-18 22:42:14 -0800

    ngepush

--- resources/views/admin/pesanan/create.blade.php
+++ resources/views/admin/pesanan/create.blade.php
@@ -68,7 +68,7 @@
             <div class="page-header">
                 <h3 class="page-title">
                     <span class="page-title-icon bg-gradient-primary text-white me-2">
-                        <i class="mdi mdi-home"></i>
+                        <i class="mdi mdi-clipboard-text menu-icon"></i>
                     </span>
                     Pesanan {{ Str::upper(Auth::user()->role) }}
                 </h3>
